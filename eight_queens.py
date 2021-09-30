4111bc0644a3af78746227e6a304b7ec2385565f
ernesto <ernesto.oliveira@inf.ufrgs.br>
2021-09-30 18:15:01 -0300

    Implement crossover()

--- eight_queens.py
+++ eight_queens.py
@@ -11,24 +11,11 @@ def evaluate(individual):
 
 
 def tournament(participants):
-    return max(participants, key=evaluate())
+    return min(participants, key=lambda x:evaluate(x))
 
 
-def crossover(parent1, parent2, index):
-    """
-    Realiza o crossover de um ponto: recebe dois indivíduos e o ponto de
-    cruzamento (indice) a partir do qual os genes serão trocados. Retorna os
-    dois indivíduos com o material genético trocado.
-    Por exemplo, a chamada: crossover([2,4,7,4,8,5,5,2], [3,2,7,5,2,4,1,1], 3)
-    deve retornar [2,4,7,5,2,4,1,1], [3,2,7,4,8,5,5,2].
-    A ordem dos dois indivíduos retornados não é importante
-    (o retorno [3,2,7,4,8,5,5,2], [2,4,7,5,2,4,1,1] também está correto).
-    :param parent1:list
-    :param parent2:list
-    :param index:int
-    :return:list,list
-    """
-    raise NotImplementedError  # substituir pelo seu codigo
+def crossover(a,b,index): 
+    return a[:index]+b[index:],b[:index]+a[index:]
 
 
 def mutate(individual, m):
